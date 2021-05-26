a466927b9f018701ecfdec52fa7ba4aa36b835a4
douglaslope <bruno.labenu@gmail.com>
2021-05-25 23:05:04 -0300

    exercicio aula 4 - operadores

---
+++ EXERCICIO-AULA4/operadores/quinzena1/index.js
@@ -0,0 +1,78 @@
+// Exercícios de interpretação de código
+// 1. 
+// a. false
+// b. false
+// c. true
+// d. boolean
+
+// 2.
+// Repetição dos dois números lado a lado
+
+// 3.
+// Insefir Number antes do prompt
+
+// Exercícios de escrita de código
+// 1.
+// // a)
+
+// const idadeUser = Number (prompt ("Qual sua idade?"))
+// console.log ("Sua idade é:", idadeUser)
+
+// // b)
+
+// const idadeAmigo = Number (prompt ("Qual idade do seu melhor amigo?"))
+// console.log ("A idade do seu melhor amigo é:", idadeAmigo)
+
+// // c)
+    
+// console.log ("Sua idade é maior do que a do seu melhor amigo?", idadeUser > idadeAmigo)
+
+// // d)
+
+// idadeDif =   idadeAmigo - idadeUser
+// console.log("a diferença de idade é de", idadeDif, "anos.")
+
+// 2.
+// // a)
+
+// const numPar = Number (prompt("Insira um número par:"))
+// const restoDivisao = numPar % 2
+
+// // b)
+
+// console.log("O resto da divisão é igual:", restoDivisao)
+
+// // c)
+
+// // Em uma divisão de um número par por 2 sempre irá retornar 0 como resto da divisão
+
+// // d)
+
+// // Em uma divisão de um número impar por 2 sempre irá retornar 1 como resto da divisão
+
+// // 3.
+// // a)
+
+// const idadeUser3 = Number (prompt ("Digite sua idade:"))
+// console.log ("Sua idade em meses é:", idadeUser3 * 12,"meses")
+
+// // b)
+
+// console.log ("Sua idade em dias é:", idadeUser3 * 365,"dias")
+
+// // c)
+
+// console.log ("Sua idade em horas é:", idadeUser3 * 8760,"horas")
+
+// // 4.
+
+// const numero1 = Number (prompt ("Digite o primeiro número:"))
+// const numero2 = Number (prompt ("Digite o segundo número:"))
+
+// console.log("Primeiro número:", numero1)
+// console.log("Segundo número:", numero2)
+
+// console. log ("O primeiro numero é maior que segundo?", numero1 > numero2 ) //true
+// console. log ("O primeiro numero é igual ao segundo?", numero1 === numero2) //false
+// console. log ("O primeiro numero é divisível pelo segundo?", (numero1 % numero2) === 0) //true
+// console. log ("O segundo numero é divisível pelo primeiro?", (numero2 % numero1) !== 0) //true
