15a117ad73c5d6ea4340b17e99b6b3a9ec68aa01
douglaslope <dougui.guilherme17@gmail.com>
2021-06-02 22:19:16 -0300

    envio-functions

---
+++ quinzena1/funcoes/index.js
@@ -0,0 +1,50 @@
+// Exercícios de interpretação de código
+// 1.
+// a)
+    // 10
+    // 50
+
+// b) nada aconteceria
+
+// 2.
+// a) Verifica se tem a palavra "cenoura" na frase e retorna true ou false. 
+// b) i. true
+//    ii. true
+//    iii. false
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Exercícios de escrita de código
+// 1.
+// // a)
+// function semparametro (){
+//     const nome = "Douglas"
+//     const idade = "29"
+//     const cidade = "João Pessoa"
+//     const profissao = "estudante"
+//     console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`)
+// }
+
+// semparametro()
+
+// // b)
+
+// const nome = prompt("Qaul seu nome?")
+// const idade = Number(prompt("Qaul sua idade?"))
+// const cidade = prompt("Qaul sua cidade?")
+// const profissao = prompt("Qaul sua profissão?")
+
+// function semparametro (){
+//     console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`)
+// }
+
+// semparametro()
+
+// 2.
+// // a)
+// function somar (a, b){
+//     const soma = a + b
+//     console.log(soma)
+// }
+// somar(2,4)
+
