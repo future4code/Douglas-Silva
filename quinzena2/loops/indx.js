123aa8253f372ff223bbdf1934af7346755b5471
douglaslope <dougui.guilherme17@gmail.com>
2021-06-10 23:45:42 -0300

    exercicio-loop

---
+++ quinzena2/loops/indx.js
@@ -0,0 +1,33 @@
+// Exercícios de interpretação de código
+// 1.
+// Somando o ultimo valor de i mais o novo i assumido até que i seja menor que 5, não
+// 2.
+// a-
+// Percorre o array para números maiores que 18.
+// 19
+// 21
+// 23
+// 25
+// 27
+// 30
+
+// b-
+
+// 3. 4 linhas contendo * onde em cada linha tem sua respectiva quantidade, na linha 4 tem 4* na linha n tem n*
+
+// Exercícios de escrita de código
+
+// // 1.
+// let arrayBichos = []
+// let novoBicho = ""
+// const quantosBichos = Number(prompt("Quantos bichos de estimação você tem?"))
+// if (quantosBichos === 0) {
+//     console.log("Que pena! Você pode adotar um pet!")    
+// }else {
+//     for (let i=0; i<quantosBichos; i++) {
+//         novosPets = prompt("Inserir nome de um Pet:")
+//         arrayBichos.push(novosPets)
+    
+//     }
+// }
+// console.log(arrayBichos,[])
