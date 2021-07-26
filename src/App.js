9e7fb16d761326785e634513da8413e05dc64610
douglaslope <dougui.guilherme17@gmail.com>
2021-07-25 23:58:34 -0300

    projeto-whatslab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import styled from "styled-components"
 
-export default App;
+const ContainerGeral = styled.div`
+max-width: 600px;
+height: 100vh;
+border: 1px solid black;
+flex: 1 1 0%;
+display: flex;
+flex-direction: column
+`;
+
+const campoImput = styled.div`
+justify-content: space-between;
+`;
+
+const usuarios = styled.div`
+display: flex;
+width: 80px;
+justify-content: space-between;
+`;
+
+export default class App extends React.Component {
+  state = {
+    usuarios: [],
+    inputNome: "",
+    inputMensagem: ""
+  };
+
+  onChangeNome = (e) => {
+    this.setState({ inputNome: e.target.value });
+  };
+
+  onChangeMensagem = (e) => {
+    this.setState({ inputMensagem: e.target.value });
+  };
+
+  adicionarUsuario = () => {
+    const novosUsuarios = [...this.state.usuarios];
+    novosUsuarios.push({
+      nome: this.state.inputNome,
+      mensagem: this.state.inputMensagem
+    });
+
+    this.setState({
+      usuarios: novosUsuarios,
+      inputNome: "",
+      inputMensagem: ""
+    });
+  };
+
+  deletarUsuario = (nomeUsuario) => {
+    const novosUsuarios = [...this.state.usuarios];
+
+    const usuariosFiltrados = novosUsuarios.filter(
+      (pessoa) => pessoa.nome !== nomeUsuario
+    );
+
+    // this.setState({instrutores: instrutoresFiltrados})
+  };
+
+  listaComponentes = () =>
+    this.state.usuarios.map((pessoa, indice) => {
+      return (
+        <usuarios key={indice}>
+          <p>{pessoa.nome}</p>
+          <p>{pessoa.mensagem}</p>
+          <button onClick={() => this.deletarUsuario(pessoa.nome)}>Apagar mensagem</button>
+        </usuarios>
+      );
+    });
+
+  render() {
+    console.log(typeof this.deletarUsuario("Teste"));
+    console.log(typeof (() => this.deletarUsuario("Teste")));
+    return (
+      <ContainerGeral className="App">
+        <h1>WhatsLab</h1>
+        <campoImput>
+          <input
+            value={this.state.inputNome}
+            onChange={this.onChangeNome}
+            placeholder="Nome"
+            />
+          <input
+            value={this.state.inputMensagem}
+            onChange={this.onChangeMensagem}
+            placeholder="Mensagem"
+            />
+          <button onClick={this.adicionarUsuario}>Enviar</button>
+          {this.listaComponentes()}
+        </campoImput>
+      </ContainerGeral>
+    );
+  }
+}
