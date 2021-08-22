1f03b078d2a1c8d23c573f7403fc2646ee2d759b
douglaslope <dougui.guilherme17@gmail.com>
2021-08-22 19:25:26 -0300

    proj-labefy-add.surge

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default class App extends React.Component{
         return <div>Pádgina não encontrada</div>
     }
   }
+  
   irParaCriarPlaylist = () => {
     this.setState({telaAtual: "criarPlaylist"})
   }
